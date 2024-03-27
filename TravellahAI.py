ae8ae2dd09b77720f39df1c2e19afe085e1ef366
iyedowh <harithkamlyzan@gmail.com>
2024-03-27 14:03:21 +0800

    1

--- TravellahAI.py
+++ TravellahAI.py
@@ -13,7 +13,7 @@ def main():
     col1, col2, col3, col4, col5 = st.columns([2, 2, 4, 2, 2])  
 
     with col3:
-        st.title(':blue[TRAVELLAH!]')
+        st.title(':dark blue[TRAVELLAH!]')
 
     origin = st.text_input('Origin')
     destination = st.text_input('Destination')
