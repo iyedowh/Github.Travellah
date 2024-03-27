565e7ff525b21ec4f69236da934366ca890bdc6e
iyedowh <harithkamlyzan@gmail.com>
2024-03-27 12:57:01 +0800

    1

--- TravellahMethod.py
+++ TravellahMethod.py
@@ -51,15 +51,15 @@ class TravellahMethod:
 
         response = client.chat.completions.create(
             model='gpt-3.5-turbo',
-            max_tokens=1000,
-            temperature=1,
+            max_tokens=2000,
+            temperature=0.5,
             messages= [
                 {
                     "role":"system",
                     "content":"""You are designed to help users estimate the budget and expenses needed for traveling between countries,
         you will describe the flight price in user's destination currency, accommodations cost in user's destination currency, meals cost in user's destination currency,
         transportation cost in user's destination currency, activities cost in user's destination currency, and overall estimated cost in user's destination currency.
-        use Times New Roman font to describe the output"""
+        you will describe all the costs in details and use Times New Roman font to describe the output"""
                 },
                 {
                     "role":"user",
