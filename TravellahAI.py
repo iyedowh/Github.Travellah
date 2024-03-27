43d5b647260e189be2924dab16fed92b11e42330
iyedowh <harithkamlyzan@gmail.com>
2024-03-27 12:23:13 +0800

    1

--- TravellahAI.py
+++ TravellahAI.py
@@ -15,7 +15,7 @@ def main():
     origin = st.text_input('Origin')
     destination = st.text_input('Destination')
     num_travellers = st.slider('Number of Travellers', min_value=1, max_value=30, value=1, step=1)
-    duration = st.slider('Duration (in days)', min_value=1, max_value=30, value=1, step=1)
+    duration = st.slider('Travel Durations (in days)', min_value=1, max_value=30, value=1, step=1)
     budget_level = st.selectbox(
     'Budget Level?',
     ('Low', 'Standard', 'High'))
@@ -27,19 +27,14 @@ def main():
     st.markdown("## Estimated Budget Details")
 
     st.write(f"**Estimated overall budget for {num_travellers} travelers from {origin} to {destination} for {duration} days with a {budget_level} budget level is: ${total_expenses}")
-    col1, col2 = st.columns(2)
+    col1 = st.columns(1)
  
     with col1:
             st.write("Travel Details:")
             st.write(f"- Origin: {origin}")
             st.write(f"- Destination: {destination}")
-    with col2:
-            st.write("Budget Information:")
             st.write(f"- Budget Level: {budget_level}")
-            st.write(f"- Total Expenses: ${total_expenses}")
-
-            st.write("Here is the breakdown of estimated budget:")
-            st.write(f"Total Expenses: ${total_expenses}", unsafe_allow_html=True)
+            st.write(f"- Travel Durations (in days): {duration}")
 
     output= tm.main_program(client, origin, destination, num_travellers, duration, budget_level ) 
     st.write(output)
