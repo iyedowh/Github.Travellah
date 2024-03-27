f09a352c20f4d33ceb1defebfbbcc417c7b9bee3
iyedowh <harithkamlyzan@gmail.com>
2024-03-27 18:54:54 +0800

    1

--- TravellahAI.py
+++ TravellahAI.py
@@ -29,7 +29,7 @@ def main():
    
     if st.button("Estimate Budget"):
             total_expenses = (origin, destination, num_travellers, duration, budget_level)
-            st.write(f"Estimating overall budget for {num_travellers} travelers from {origin} to {destination} for {duration} days with a {budget_level}. Let me calculate for you, please wait for a second.")
+            st.write(f"Estimating overall budget for {num_travellers} travelers from {origin} to {destination} for {duration} days with a {budget_level} budget level. Let me calculate for you, please wait for a second okiee.")
             output= tm.main_program(client, origin, destination, num_travellers, duration, budget_level ) 
             st.write(output)
 
