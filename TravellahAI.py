ffb1c0904814e04ddbf78350653459cf9a444a48
iyedowh <harithkamlyzan@gmail.com>
2024-03-29 07:52:56 +0800

    1

--- TravellahAI.py
+++ TravellahAI.py
@@ -58,6 +58,8 @@ def main():
             
             total_expenses = (origin, destination, num_travellers, duration, budget_level)
 
+            st.divider()
+
             st.write(f"""Estimating overall budget for {num_travellers} travelers
                     from {origin} to {destination} with a {budget_level} budget level. 
                     Let me calculate for you, please wait for a second okiee.""")
