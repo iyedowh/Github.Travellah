712cf36309e7920a265c13568f1009f7b00db85c
iyedowh <harithkamlyzan@gmail.com>
2024-03-29 07:32:13 +0800

    1

--- TravellahAI.py
+++ TravellahAI.py
@@ -1,5 +1,6 @@
 import os
 from openai import OpenAI 
+import datetime
 import streamlit as st
 from TravellahMethod import TravellahMethod as tm
 
@@ -31,8 +32,19 @@ def main():
     num_travellers = st.slider('Number of Travellers', min_value=1, max_value=30, value=1, step=1)
 
     st.divider()
+    
+    today = datetime.datetime.now()
+    next_year = today.year + 2
+    jan_1 = datetime.date(today, 1, 1)
+    dec_31 = datetime.date(next_year, 12, 31)
 
-    duration = st.slider('Travel Durations (in days)', min_value=1, max_value=30, value=1, step=1)
+    duration = st.date_input(
+        "Select your travelling time: ",
+        (jan_1, datetime.date(next_year, 1, 7)),
+        jan_1,
+        dec_31,
+        format="MM.DD.YYYY",
+    )
 
     st.divider()
 
@@ -47,7 +59,7 @@ def main():
             total_expenses = (origin, destination, num_travellers, duration, budget_level)
 
             st.write(f"""Estimating overall budget for {num_travellers} travelers
-                    from {origin} to {destination} for {duration} days with a {budget_level} budget level. 
+                    from {origin} to {destination} with a {budget_level} budget level on {date}. 
                     Let me calculate for you, please wait for a second okiee.""")
             
             st.divider()
